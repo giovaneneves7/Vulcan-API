d076d2eba75f70702df5a1ddfe02bd0b4ee7fc71
NekoYasha <autornekoyasha@gmail.com>
2023-05-14 13:55:16 -0300

    Edita metodo salvar

--- src/main/java/br/com/vulcan/jvulcan/api/novel/service/NovelService.java
+++ src/main/java/br/com/vulcan/jvulcan/api/novel/service/NovelService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NovelService implements INovelService
@@ -47,27 +48,81 @@ public class NovelService implements INovelService
     {
         List<Novel> novels = listarTodas();
 
+        //--+ Verifica se há registro no banco de dados +--//
+        if(novels.isEmpty())
+        {
+            novel.setColocacao(1);
+            novel.setColocacaoMensal(1);
+            novelRepository.save(novel);
+
+            return true;
+        }
+
         boolean slugExiste = novels.stream()
                                       .anyMatch(n -> n.getSlug().equals(novel.getSlug()));
         boolean indiceExiste = novels.stream()
                 .anyMatch(n -> n.getSlug().equals(novel.getIndice()));
 
 
-        //--+ Define o rank da novel +--//
+        //--+ Organiza a novel de acordo com as views +--//
         if(!slugExiste && !indiceExiste)
         {
             novels.add(novel);
 
-            novels.sort((n1, n2) -> Integer.compare(n2.getViewsTotais(), n1.getViewsTotais()));
+            reorganizarPorViewsMensais(novels);
+            reorganizarPorViewsTotais(novels);
 
-            for(int i = 0; i < novels.size(); i++)
+            for(Novel nvl : novels)
             {
-                novels.get(i).setColocacao((i + 1));
+                Optional<Novel> existeNoBanco = novelRepository.findById((long) nvl.getColocacao());
+
+                if(existeNoBanco.isPresent())
+                {
+                    Novel novelAtualizada = existeNoBanco.get();
+                    novelAtualizada.setColocacaoMensal(nvl.getColocacaoMensal());
+                    novelAtualizada.setViewsTotais(nvl.getViewsTotais());
+                    novelAtualizada.setViewsMensais(nvl.getViewsMensais());
+                    novelAtualizada.setSlug(nvl.getSlug());
+                    novelAtualizada.setNome(nvl.getNome());
+                    novelAtualizada.setNacionalidade(nvl.getNacionalidade());
+                    novelAtualizada.setCapa(nvl.getCapa());
+                    novelAtualizada.setIndice(nvl.getIndice());
+                    novelAtualizada.setAutor(nvl.getAutor());
+                    novelAtualizada.setQuantidadeCapitulos(nvl.getQuantidadeCapitulos());
+                    novelAtualizada.setCargo(nvl.getCargo());
+                    novelAtualizada.setGeneros(nvl.getGeneros());
+                    novelAtualizada.setEscritor(nvl.getEscritor());
+                    novelAtualizada.setEstrelas(nvl.getEstrelas());
+                    novelAtualizada.setDataCriacaoIndice(nvl.getDataCriacaoIndice());
+                    novelAtualizada.setSinopse(nvl.getSinopse());
+                    this.novelRepository.save(novelAtualizada);
+                } else{
+                    this.novelRepository.save(nvl);
+                }
             }
-            this.novelRepository.saveAll(novels);
+
             return true;
         }
 
+
         return false;
+    }
+
+    private void reorganizarPorViewsMensais(List<Novel> novels)
+    {
+        for(int i = 0; i < novels.size(); i++)
+        {
+            novels.get(i).setColocacaoMensal(i + 1);
+        }
+    }
+
+    private void reorganizarPorViewsTotais(List<Novel> novels)
+    {
+        novels.sort((n1, n2) -> Integer.compare(n2.getViewsTotais(), n1.getViewsTotais()));
+
+        for(int i = 0; i < novels.size(); i++)
+        {
+            novels.get(i).setColocacao((i + 1));
+        }
     }
 }
