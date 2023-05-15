0b823b1ca00ad09fec9a9846ec60898726414fdd
NekoYasha <autornekoyasha@gmail.com>
2023-05-15 20:31:17 -0300

    Adiciona metodo de encontrar por nome

--- src/main/java/br/com/vulcan/jvulcan/api/banners/repository/BannerRepository.java
+++ src/main/java/br/com/vulcan/jvulcan/api/banners/repository/BannerRepository.java
@@ -4,8 +4,12 @@ import br.com.vulcan.jvulcan.api.banners.model.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BannerRepository extends JpaRepository<Banner, Long>
 {
+
+    Optional<Banner> findBannerByNome(String nome);
 
 }
