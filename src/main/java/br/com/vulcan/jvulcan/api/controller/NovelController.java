dbc048a1c9c466b38629f36c03e7589a76db0c2e
NekoYasha <autornekoyasha@gmail.com>
2023-05-15 14:36:11 -0300

    Renomeia endpoint do request mapping

--- src/main/java/br/com/vulcan/jvulcan/api/controller/NovelController.java
+++ src/main/java/br/com/vulcan/jvulcan/api/controller/NovelController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "nekoyasha7/api/jvulcan1000-beta")
+@RequestMapping(path = "nekoyasha7/jvulcan-api/v1")
 public class NovelController
 {
 
