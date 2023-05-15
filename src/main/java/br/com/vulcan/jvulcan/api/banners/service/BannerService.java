ea22c2b6f6e8d0449f28a30173d4240b85e6e5eb
NekoYasha <autornekoyasha@gmail.com>
2023-05-15 14:34:43 -0300

    Cria service de Banner

--- src/main/java/br/com/vulcan/jvulcan/api/banners/service/BannerService.java
+++ src/main/java/br/com/vulcan/jvulcan/api/banners/service/BannerService.java
@@ -1,2 +1,35 @@
-package br.com.vulcan.jvulcan.api.banners.service;public class BannerService {
+package br.com.vulcan.jvulcan.api.banners.service;
+
+import br.com.vulcan.jvulcan.api.banners.model.Banner;
+import br.com.vulcan.jvulcan.api.banners.repository.BannerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class BannerService implements IBannerService
+{
+
+    @Autowired
+    BannerRepository bannerRepository;
+
+
+    /**
+     * Retorna um banner aleatório da base de dados.
+     * @return um banner aleatório.
+     */
+    @Override
+    public Banner pegarBannerAleatorio()
+    {
+
+        List<Banner> banners = bannerRepository.findAll();
+        Random rand = new Random();
+        int indiceAleatorio = rand.nextInt(banners.size());
+
+        return banners.get(indiceAleatorio);
+
+
+    }
 }
--- src/main/java/br/com/vulcan/jvulcan/api/banners/service/IBannerService.java
+++ src/main/java/br/com/vulcan/jvulcan/api/banners/service/IBannerService.java
@@ -1,2 +1,14 @@
-package br.com.vulcan.jvulcan.api.banners.service;public interface IBannerService {
+package br.com.vulcan.jvulcan.api.banners.service;
+
+import br.com.vulcan.jvulcan.api.banners.model.Banner;
+
+public interface IBannerService
+{
+
+    /**
+     * Retorna um banner aleatório da base de dados.
+     * @return um banner aleatório.
+     */
+    Banner pegarBannerAleatorio();
+
 }
