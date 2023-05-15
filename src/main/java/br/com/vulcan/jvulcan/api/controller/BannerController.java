d7486364de2aa455a03440b563d74755034b4955
NekoYasha <autornekoyasha@gmail.com>
2023-05-15 14:35:26 -0300

    Cria endpoint para pegar banner aleátorio

--- src/main/java/br/com/vulcan/jvulcan/api/controller/BannerController.java
+++ src/main/java/br/com/vulcan/jvulcan/api/controller/BannerController.java
@@ -1,2 +1,26 @@
-package br.com.vulcan.jvulcan.api.controller;public class BannerController {
+package br.com.vulcan.jvulcan.api.controller;
+
+import br.com.vulcan.jvulcan.api.banners.model.Banner;
+import br.com.vulcan.jvulcan.api.infrastructure.service.IFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "nekoyasha7/jvulcan-api/v1")
+public class BannerController
+{
+
+    @Autowired
+    IFacade facade;
+
+    @GetMapping(path = "/banners/banner")
+    public ResponseEntity<Banner> pegarBannerAleatorio()
+    {
+
+        return ResponseEntity.ok(facade.pegarBannerAleatorio());
+
+    }
 }
