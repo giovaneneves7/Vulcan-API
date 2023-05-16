09fe4e9f6d477178e474678b1b546fb8c534ef3f
NekoYasha <autornekoyasha@gmail.com>
2023-05-15 21:55:21 -0300

    Adicinado método de cadastro de banner

--- src/main/java/br/com/vulcan/jvulcan/api/controller/BannerController.java
+++ src/main/java/br/com/vulcan/jvulcan/api/controller/BannerController.java
@@ -4,9 +4,7 @@ import br.com.vulcan.jvulcan.api.banners.model.Banner;
 import br.com.vulcan.jvulcan.api.infrastructure.service.IFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "nekoyasha7/jvulcan-api/v1")
@@ -21,6 +19,19 @@ public class BannerController
     {
 
         return ResponseEntity.ok(facade.pegarBannerAleatorio());
+
+    }
+
+    @PostMapping(path = "banners/banner")
+    public ResponseEntity<String> salvarBanner(@RequestBody Banner banner)
+    {
+
+        if(!this.facade.salvarBanner(banner))
+        {
+            return ResponseEntity.badRequest().body("Já existe um banner com este nome!");
+        }
+
+        return ResponseEntity.ok("Banner cadastrado com successo!");
 
     }
 }
