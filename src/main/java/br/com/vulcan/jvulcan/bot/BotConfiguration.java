29c003196e2a2e0ea62a28be87f00bbbdf78caed
NekoYasha <autornekoyasha@gmail.com>
2023-05-17 11:56:52 -0300

    Cria configuração do bot

--- src/main/java/br/com/vulcan/jvulcan/bot/BotConfiguration.java
+++ src/main/java/br/com/vulcan/jvulcan/bot/BotConfiguration.java
@@ -1,2 +1,22 @@
-package br.com.vulcan.jvulcan.bot;public class BotConfiguration {
+package br.com.vulcan.jvulcan.bot;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+import javax.security.auth.login.LoginException;
+
+@Configuration
+public class BotConfiguration {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @PostConstruct
+    public void initializeBot() throws LoginException {
+        BotLauncher botLauncher = applicationContext.getBean(BotLauncher.class);
+        botLauncher.iniciarBot();
+    }
 }
+
