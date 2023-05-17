2849ada6f0b87e384b6fe29e0036ad358306fae5
NekoYasha <autornekoyasha@gmail.com>
2023-05-17 10:57:34 -0300

    Adiciona anotacao Value

--- src/main/java/br/com/vulcan/jvulcan/bot/BotLauncher.java
+++ src/main/java/br/com/vulcan/jvulcan/bot/BotLauncher.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.security.auth.login.LoginException;
@@ -12,7 +13,8 @@ import javax.security.auth.login.LoginException;
 public class BotLauncher extends ListenerAdapter
 {
 
-    private final String TOKEN = "MEU_TOKEN";
+    @Value("${token}")
+    private String TOKEN;
 
     public BotLauncher() throws LoginException
     {
