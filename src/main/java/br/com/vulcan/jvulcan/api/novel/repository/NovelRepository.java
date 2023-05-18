b0596046fb52e488cdc707e4f9607598ee488450
NekoYasha <autornekoyasha@gmail.com>
2023-05-18 14:03:05 -0300

    Adiciona buscar por nacionalidade

--- src/main/java/br/com/vulcan/jvulcan/api/novel/repository/NovelRepository.java
+++ src/main/java/br/com/vulcan/jvulcan/api/novel/repository/NovelRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface NovelRepository extends JpaRepository<Novel, Long>
 {
     List<Novel> findByNacionalidade(String nacionalidade);
+    Optional<Novel> findBySlug(String slug);
 }
