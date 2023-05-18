175f322936b6f8dad3595dd8707343a721288ae1
NekoYasha <autornekoyasha@gmail.com>
2023-05-18 14:01:49 -0300

    Adiciona requestParams

--- src/main/java/br/com/vulcan/jvulcan/api/controller/v1/NovelController.java
+++ src/main/java/br/com/vulcan/jvulcan/api/controller/v1/NovelController.java
@@ -1,4 +1,4 @@
-package br.com.vulcan.jvulcan.api.controller;
+package br.com.vulcan.jvulcan.api.controller.v1;
 
 import br.com.vulcan.jvulcan.api.infrastructure.service.IFacade;
 import br.com.vulcan.jvulcan.api.novel.model.Novel;
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,15 +21,44 @@ public class NovelController
     /**
      * Retorna uma lista com todas as novels na base de dados.
      * @param nacionalidade (Opicional) A nacionalidade das novels que serão retornadas.
-     * @return Lista de novels da base de dados.
+     * @return Lista de novels da base de dados segundo os parametros passados.
      */
     @GetMapping(path = "/novels")
-    public ResponseEntity<List<Novel>> listarTodas(@RequestParam(name = "nacionalidade", required = false) String nacionalidade)
+    public ResponseEntity<List<Novel>> listarNovels(@RequestParam(name = "nacionalidade", required = false) String nacionalidade,
+                                                    @RequestParam(name = "tipo", required = false) String tipo)
     {
 
+        //--+ Retorna lista com nacionalidade especificada +--//
         if(nacionalidade != null)
         {
             return ResponseEntity.ok(this.facade.listarTodasNovels(nacionalidade));
+        }
+
+        //--+ Retorna lista com tipo passado por parametro+--//
+        if(tipo != null)
+        {
+            List<Novel> novelsRetornadas = new ArrayList<>();
+
+            switch (tipo)
+            {
+                case "traducoes":
+
+                    novelsRetornadas.addAll(this.facade.listarTodasNovels("oci"));
+                    novelsRetornadas.addAll(this.facade.listarTodasNovels("ch"));
+                    novelsRetornadas.addAll(this.facade.listarTodasNovels("co"));
+                    novelsRetornadas.addAll(this.facade.listarTodasNovels("jp"));
+
+                    if(!novelsRetornadas.isEmpty())
+                        return ResponseEntity.ok(novelsRetornadas);
+
+                case "originais":
+
+                    novelsRetornadas.addAll(this.facade.listarTodasNovels("br"));
+
+                    if(!novelsRetornadas.isEmpty())
+                        return ResponseEntity.ok(novelsRetornadas);
+
+            }
         }
 
         return ResponseEntity.ok(this.facade.listarTodasNovels());
