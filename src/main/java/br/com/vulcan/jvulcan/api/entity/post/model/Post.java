af33f55616a476688d6ac42b288c19edf50fd439
NekoYasha <autornekoyasha@gmail.com>
2023-05-19 12:59:37 -0300

    Cria class Post Criada classe para receber dados da postagem

---
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/post/model/Post.java
@@ -0,0 +1,29 @@
+package br.com.vulcan.jvulcan.api.entity.post.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Data
+public class Post
+{
+
+    @JsonProperty("nome_novel")
+    private String nomeNovel;
+
+    @JsonProperty("titulo_postagem")
+    private String titulo;
+
+    @JsonProperty("autor_postagem")
+    private String autor;
+
+    @JsonProperty("link_postagem")
+    private String link;
+
+    @JsonProperty("link_avatar_autor")
+    private String linkAvatarAutor;
+
+    private String cargoMarcado;
+}
---
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/post/service/IPostService.java
@@ -0,0 +1,2 @@
+package br.com.vulcan.jvulcan.api.entity.post.service;public class IPostService {
+}
