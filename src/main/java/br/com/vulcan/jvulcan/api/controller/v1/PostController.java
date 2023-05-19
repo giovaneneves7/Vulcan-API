81787dab64fd975a2a3ea40b336f254f3c23d50a
NekoYasha <autornekoyasha@gmail.com>
2023-05-19 14:10:34 -0300

    Adiciona chamada ao metodo de pegar novos lancamentos Adicionado metodo de pegar novas postagens no endpoint /posts/post

--- src/main/java/br/com/vulcan/jvulcan/api/controller/v1/PostController.java
+++ src/main/java/br/com/vulcan/jvulcan/api/controller/v1/PostController.java
@@ -1,20 +1,24 @@
 package br.com.vulcan.jvulcan.api.controller.v1;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.vulcan.jvulcan.api.entity.post.model.Post;
+import br.com.vulcan.jvulcan.api.infrastructure.service.IFacade;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "nekoyasha7/jvulcan-api/v1")
 public class PostController
 {
 
-    @GetMapping("/posts/recentes")
-    public void pegarPostsRecentes()
+    @Autowired
+    IFacade facade;
+
+    @PostMapping("/posts/post")
+    public void pegarPostsRecentes(@RequestBody Post post)
     {
 
-
+        this.facade.notificarNovaPostagem(post);
 
     }
 
