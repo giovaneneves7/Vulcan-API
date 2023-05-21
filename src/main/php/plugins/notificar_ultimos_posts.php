bb7861cdf077b9ef42f179e49e19f498e6b2b2d0
NekoYasha <autornekoyasha@gmail.com>
2023-05-21 13:10:41 -0300

    Conserta bugs do plugin Consertado o bug de não identificar o nome do autor do post e o bug de requisições repetidas ao endpoint

--- src/main/php/plugins/notificar_ultimos_posts.php
+++ src/main/php/plugins/notificar_ultimos_posts.php
@@ -20,6 +20,23 @@
             );
             $posts_recentes = get_posts($args);
 
+            //--+ Pega o ID do post +--//
+            $post_id = get_the_ID();
+
+            //--+ Pega a categoria da postagem +--//
+            $categorias = get_the_category($post_id);
+            $categoria_post = "";
+
+            //--+ Se houver categoria, pega a primeira +--//
+            if($categorias)
+            {
+                $categoria_post = $categorias[0]->name;
+            }
+
+
+            //--+ Pega o nome do autor do post +--//
+            $autor_nome = get_the_author($post_id);
+
             if ($posts_recentes)
             {
                 $post = $posts_recentes[0];
@@ -28,9 +45,10 @@
                 $info_post = array(
                     'nome_novel' => $post->post_title,
                     'titulo_postagem' => $post->post_title,
-                    'autor_postagem' => $post->post_author,
+                    'autor_postagem' => $autor_nome,
                     'link_postagem' => get_permalink($post),
                     'link_avatar_autor' => get_avatar_url($post->post_author),
+                    'categoria' => $categoria_post,
                 );
 
                 // Configurar as opções da requisição
@@ -43,7 +61,7 @@
                 );
 
                 // Fazer a requisição para o endpoint
-                $response = wp_remote_request('MEU_ENDPOINT', $configs_requisicao);
+                $response = wp_remote_request('https://d1aa-45-181-199-212.ngrok-free.app/nekoyasha7/jvulcan-api/v1/posts/post', $configs_requisicao);
 
                 // Verificar se a requisição foi bem-sucedida
                 if (is_wp_error($response)) {
