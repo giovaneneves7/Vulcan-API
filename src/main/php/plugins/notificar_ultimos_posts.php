9c9e21e67e5f9141a5fa137f44731c7a2ed3f167
NekoYasha <autornekoyasha@gmail.com>
2023-05-22 11:35:02 -0300

    Adiciona verificacao de post Adicionado verificação para comparar o post que será postado com a último post.

--- src/main/php/plugins/notificar_ultimos_posts.php
+++ src/main/php/plugins/notificar_ultimos_posts.php
@@ -9,8 +9,12 @@
      * Author URI: https://github.com/NeveScript
      * Text Domain: notificar-novos-posts
      */
+
+    $ultimo_post_notificado = "";
     function enviar_post_recente_para_endpoint()
     {
+
+        global $ultimo_post_notificado;
 
         if(is_admin())
         {
@@ -41,41 +45,47 @@
             {
                 $post = $posts_recentes[0];
 
-                // Montar os dados do post para enviar no corpo da requisição
-                $info_post = array(
-                    'nome_novel' => $post->post_title,
-                    'titulo_postagem' => $post->post_title,
-                    'autor_postagem' => $autor_nome,
-                    'link_postagem' => get_permalink($post),
-                    'link_avatar_autor' => get_avatar_url($post->post_author),
-                    'categoria' => $categoria_post,
-                );
+                if($post_id != $ultimo_post_notificado)
+                {
+                    // Montar os dados do post para enviar no corpo da requisição
+                    $info_post = array(
+                        'nome_novel' => $post->post_title,
+                        'titulo_postagem' => $post->post_title,
+                        'autor_postagem' => $autor_nome,
+                        'link_postagem' => get_permalink($post),
+                        'link_avatar_autor' => get_avatar_url($post->post_author),
+                        'categoria' => $categoria_post,
+                    );
 
-                // Configurar as opções da requisição
-                $configs_requisicao = array(
-                    'method' => 'POST',
-                    'body' => json_encode($info_post),
-                    'headers' => array(
-                        'Content-Type' => 'application/json',
-                    ),
-                );
+                    // Configurar as opções da requisição
+                    $configs_requisicao = array(
+                        'method' => 'POST',
+                        'body' => json_encode($info_post),
+                        'headers' => array(
+                            'Content-Type' => 'application/json',
+                        ),
+                    );
 
-                // Fazer a requisição para o endpoint
-                $response = wp_remote_request('https://d1aa-45-181-199-212.ngrok-free.app/nekoyasha7/jvulcan-api/v1/posts/post', $configs_requisicao);
+                    // Fazer a requisição para o endpoint
+                    $response = wp_remote_request('https://20f7-45-181-199-212.ngrok-free.app/nekoyasha7/jvulcan-api/v1/posts/post', $configs_requisicao);
 
-                // Verificar se a requisição foi bem-sucedida
-                if (is_wp_error($response)) {
-                    error_log('Erro na requisição: ' . $response->get_error_message());
-                } else {
-                    $response_code = wp_remote_retrieve_response_code($response);
-                    if ($response_code === 200) {
-                        // Requisição bem-sucedida
-                        error_log('Requisição enviada com sucesso!');
+                    // Verificar se a requisição foi bem-sucedida
+                    if (is_wp_error($response)) {
+                        error_log('Erro na requisição: ' . $response->get_error_message());
                     } else {
-                        // Requisição retornou um código de erro
-                        error_log('Erro na requisição. Código de resposta: ' . $response_code);
+                        $response_code = wp_remote_retrieve_response_code($response);
+                        if ($response_code === 200) {
+                            // Requisição bem-sucedida
+                            error_log('Requisição enviada com sucesso!');
+                        } else {
+                            // Requisição retornou um código de erro
+                            error_log('Erro na requisição. Código de resposta: ' . $response_code);
+                        }
                     }
+
+                    $ultimo_post_notificado = $post_id;
                 }
+
             }
         }
 
