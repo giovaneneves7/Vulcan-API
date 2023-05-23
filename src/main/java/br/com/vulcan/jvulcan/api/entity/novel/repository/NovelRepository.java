0283a40dd5cb9f18d63e119215924ad8732b3ef3
NekoYasha <autornekoyasha@gmail.com>
2023-05-22 21:37:22 -0300

    Adiciona comentários sobre as funções da classe

--- src/main/java/br/com/vulcan/jvulcan/api/entity/novel/repository/NovelRepository.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/novel/repository/NovelRepository.java
@@ -10,7 +10,25 @@ import java.util.Optional;
 @Repository
 public interface NovelRepository extends JpaRepository<Novel, Long>
 {
+
+    /**
+     * Retorna lista de novels com a nacionalidade especificada.
+     * @param nacionalidade A nacionalidade da novel a ser buscada.
+     * @return lista de novel com a nacionalidade especificada.
+     */
     List<Novel> findByNacionalidade(String nacionalidade);
+
+    /**
+     * Retorna uma novel com o slug passado por parâmetro.
+     * @param slug O slug da novel a ser buscada.
+     * @return novel com o slug especificado.
+     */
     Optional<Novel> findBySlug(String slug);
+
+    /**
+     * Retorna novel com o nome passado por parâmetro.
+     * @param nome O nome da novel a ser buscado.
+     * @return novel com o nome especificado.
+     */
     Optional<Novel> findByNome(String nome);
 }
