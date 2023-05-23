a68ce8f73a4cb13dd10078f1138e2c3f24c66f34
NekoYasha <autornekoyasha@gmail.com>
2023-05-22 21:11:59 -0300

    Cria pacote de exceções

--- src/main/java/br/com/vulcan/jvulcan/api/infrastructure/exception/NovelNotFoundException.java
+++ src/main/java/br/com/vulcan/jvulcan/api/infrastructure/exception/NovelNotFoundException.java
@@ -1,2 +1,24 @@
-package br.com.vulcan.jvulcan.api.infrastructure.exception;public class NovelNotFoundException {
+package br.com.vulcan.jvulcan.api.infrastructure.exception;
+
+public class NovelNotFoundException extends Exception
+{
+    public NovelNotFoundException() {
+        super();
+    }
+
+    public NovelNotFoundException(String message) {
+        super(message);
+    }
+
+    public NovelNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NovelNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NovelNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
