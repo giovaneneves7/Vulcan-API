de2b41df13a382b963156a3e27387d09fb5e4f22
NekoYasha <autornekoyasha@gmail.com>
2023-06-01 11:46:26 -0300

    Edita chave da api Parâmetro da chave da api editado para receber a chave vinda no header, não mais como query param.

--- src/main/java/br/com/vulcan/jvulcan/api/controller/v1/PostController.java
+++ src/main/java/br/com/vulcan/jvulcan/api/controller/v1/PostController.java
@@ -11,62 +11,66 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Slf4j
 @RestController
 @RequestMapping(path = "nekoyasha7/jvulcan-api/v1")
-public class PostController
-{
+public class PostController {
 
     @Value("${api_key}")
     private String API_KEY;
 
     @PostConstruct
-    public void init()
-    {
-        if(!API_KEY.isEmpty())
+    public void init() {
+        if (!API_KEY.isEmpty())
             log.info("Enpoint usando api");
     }
-
 
     @Autowired
     IFacade facade;
 
     /**
      * Notifica as novas postagens via WebHook (Discord).
-     * @param post O post que será notificado via WebHoook.
+     * 
+     * @param post     O post que será notificado via WebHoook.
+     * @param ChaveApi A chave da api.
+     * @return OK - O post foi notificado com sucesso;
+     *         UNAUTHORIZED - Sem permissão para acessar a API;
+     *         NOT_FOUND - A categoria a ser notificada não existe na base de dados;
+     *         BAD_REQUEST - A mensagem webhook não pôde ser enviada.
      */
     @PostMapping("/posts/post")
     public ResponseEntity<?> pegarPostsRecentes(@RequestBody Post post,
-                                                @RequestParam(name = "api_key") String chaveAPI)
-    {
+            @RequestHeader(name = "api_key") String chaveAPI) {
 
-        //--+ Verifica se a chave de API é válida +--//
-        if(!API_KEY.equals(chaveAPI))
-        {
+        // --+ Verifica se a chave de API é válida +--//
+        if (!API_KEY.equals(chaveAPI)) {
+
             log.info("Chave de API inválida");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body("Você não tem permissão para acessar esse recurso, bleh!");
+
         }
 
-        try
-        {
+        try {
 
             this.facade.notificarNovaPostagem(post);
             return new ResponseEntity<>(HttpStatus.OK);
 
-        } catch (ObjectNotFoundException ex)
-        {
+        } catch (ObjectNotFoundException ex) {
 
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                                 .body(ex.getMessage());
+                    .body(ex.getMessage());
 
-        } catch (MessageNotSentException ex)
-        {
+        } catch (MessageNotSentException ex) {
 
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                                 .body(ex.getMessage());
+                    .body(ex.getMessage());
         }
 
     }
