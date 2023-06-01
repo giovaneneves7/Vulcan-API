e6242fa332a487024caf19f0f89883a7f8597288
NekoYasha <autornekoyasha@gmail.com>
2023-06-01 11:53:30 -0300

    Adicona handleMessageNotSentException

--- src/main/java/br/com/vulcan/jvulcan/api/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/vulcan/jvulcan/api/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.vulcan.jvulcan.api.exception;
 
+import br.com.vulcan.jvulcan.api.infrastructure.exception.MessageNotSentException;
 import br.com.vulcan.jvulcan.api.infrastructure.exception.ObjectNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -18,24 +19,23 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler
-{
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Value(value = "${server.error.include-exception}")
     private boolean printStackTrace;
 
     @Override
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException methodArgumentNotValidException,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatusCode status,
-                                                                  WebRequest request)
-    {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException methodArgumentNotValidException,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
 
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Validation error. Check 'errors' field for details.");
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Validation error. Check 'errors' field for details.");
 
-        for (FieldError fieldError : methodArgumentNotValidException.getBindingResult().getFieldErrors())
-        {
+        for (FieldError fieldError : methodArgumentNotValidException.getBindingResult().getFieldErrors()) {
             errorResponse.addValidationError(fieldError.getField(), fieldError.getDefaultMessage());
         }
 
@@ -45,16 +45,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler
 
     /**
      * Pega todas as exceções genéricas.
+     * 
      * @param exception A exceção genérica.
-     * @param request A requisição que ocasionou o disparo da exceção.
+     * @param request   A requisição que ocasionou o disparo da exceção.
      * @return mensagem de erro.
      */
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<Object> handleAllUncaughtException(
-                                                                Exception exception,
-                                                                WebRequest request)
-    {
+            Exception exception,
+            WebRequest request) {
 
         final String mensagemErro = "Unknown error occurred";
 
@@ -67,43 +67,52 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler
                 request);
     }
 
-    //--+ Tratamento de exceções personalizadas +--//
+    // --+ Tratamento de exceções personalizadas +--//
     @ExceptionHandler(ObjectNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Object> handleObjectNotFoundException(
-                                                                ObjectNotFoundException objectNotFoundException,
-                                                                WebRequest request)
-    {
+            ObjectNotFoundException objectNotFoundException,
+            WebRequest request) {
         log.error("Failed to find the requested element", objectNotFoundException);
 
         return construirMensagemDeErro(
-                                        objectNotFoundException,
-                                        objectNotFoundException.getMessage(),
-                                        HttpStatus.NOT_FOUND,
-                                        request
-                                       );
+                objectNotFoundException,
+                objectNotFoundException.getMessage(),
+                HttpStatus.NOT_FOUND,
+                request);
     }
 
+    @ExceptionHandler(ObjectNotFoundException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleMessageNotSentException(
+            MessageNotSentException messageNotSentException,
+            WebRequest request) {
+        log.error("Failed to find the requested element ", messageNotSentException);
 
+        return construirMensagemDeErro(
+                messageNotSentException,
+                messageNotSentException.getMessage(),
+                HttpStatus.NOT_FOUND,
+                request);
+    }
 
     /**
      * Constroi uma mensagem de erro.
-     * @param exception A exceção.
-     * @param message A mensagem de exceção.
+     * 
+     * @param exception  A exceção.
+     * @param message    A mensagem de exceção.
      * @param httpStatus O status da requisição.
-     * @param request A requisição.
+     * @param request    A requisição.
      * @return a mensagem de erro.
      */
     private ResponseEntity<Object> construirMensagemDeErro(
-                                                            Exception exception,
-                                                            String message,
-                                                            HttpStatus httpStatus,
-                                                            WebRequest request)
-    {
+            Exception exception,
+            String message,
+            HttpStatus httpStatus,
+            WebRequest request) {
 
         ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
-        if (this.printStackTrace)
-        {
+        if (this.printStackTrace) {
             errorResponse.setStacktrace(ExceptionUtils.getStackTrace(exception));
         }
 
