e56271e8c7efc8bbd0bdf5a071b73d2070c6a4a5
NekoYasha <autornekoyasha@gmail.com>
2023-06-04 21:40:31 -0300

    Adiciona assinatura dos método na interface do serviço

--- src/main/java/br/com/vulcan/jvulcan/api/entity/chibata/service/IOlhoDaChibataService.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/chibata/service/IOlhoDaChibataService.java
@@ -8,8 +8,21 @@ public interface IOlhoDaChibataService
 {
 
     /**
+     * Cadastra dados de staffs e novels na base de dados.
+     * @param dadosChibata Os dados que serão cadastrados.
+     * @return 'true' caso seja cadastrado com sucesso, 'false' caso não.
+     */
+    boolean cadastrar(OlhoDaChibata dadosChibata);
+
+    /**
      * Lista todos os dados do Olho da Chibata.
      * @return Lista com dados do Olho da Chibata.
      */
     List<OlhoDaChibata> listarTodos();
+
+    /**
+     * Cobra membros com 7 dias sem postagem.
+     * @return 'true' caso a cobrança seja realizada com sucesso, 'false' caso não.
+     */
+    boolean cobrarBaianos();
 }
