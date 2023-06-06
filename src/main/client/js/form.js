4e921bfba5364eac839a71a17aedd6705b5d0846
NekoYasha <autornekoyasha@gmail.com>
2023-06-06 14:10:24 -0300

    Cria função para fazer requisição com Fetch API

--- src/main/client/js/form.js
+++ src/main/client/js/form.js
@@ -14,3 +14,83 @@ function pegarIndice() {
     document.getElementById('indice').value = slug;
 
 }
+
+function enviarFormulario() {
+
+    event.preventDefault();
+
+    var formulario = document.getElementById('novel-form');
+    var dados = {};
+
+    try {
+
+        dados.nome_projeto = formulario.querySelector('[name="nome_projeto"]').value;
+        dados.views_totais = formulario.querySelector('[name="views-totais"]').value;
+        dados.views_mensais = formulario.querySelector('[name="views-mensais"]').value;
+        dados.slug = formulario.querySelector('[name="slug"]').value;
+        dados.indice = formulario.querySelector('[name="indice"]').value;
+        dados.escritor = formulario.querySelector('[name="escritor"]').value;
+        dados.quantidade = formulario.querySelector('[name="quantidade"]').value;
+        dados.id_cargo = formulario.querySelector('[name="id_cargo"]').value;
+        dados.capa = formulario.querySelector('[name="capa"]').value;
+        dados.nacionalidade = formulario.querySelector('[name="nacionalidade"]:checked').value;
+        dados.status_projeto = formulario.querySelector('[name="status_projeto"]:checked').value;
+
+        document.getElementById('alert-empty-field').style.display = 'none';
+
+        fazerRequisicao(dados);
+
+    } catch (error) {
+
+        exibirAvisoCamposVazios();
+    }
+
+    return;
+
+}
+
+function exibirAvisoCamposVazios() {
+
+    document.getElementById('alert-empty-field').style.display = 'block';
+
+}
+
+function fazerRequisicao(dados) {
+
+    var host = "https://spring-api.thankfulriver-9e15c811.eastus.azurecontainerapps.io";
+    var url = "/nekoyasha7/jvulcan-api/v1/posts/post";
+
+    fetch(host.concat(url), {
+        method: "POST",
+        headers: {
+            "Application-Content": "application/json"
+        },
+        body: JSON.stringify(dados)
+    }
+    ).then(function (response) {
+
+        if (response.ok) {
+
+            document.getElementById('success-registration').style.display = "block";
+            document.getElementById('failure-registration').style.display = "none";
+
+
+        } else {
+
+            document.getElementById('failure-registration').style.display = "block";
+            document.getElementById('success-registration').style.display = "none";
+            throw new Error("Erro na requisição");
+
+        }
+
+    }).then(function (resposta) {
+
+        console.log(resposta);
+
+    }).catch(function (error) {
+
+        console.error(error);
+
+    });
+
+}
