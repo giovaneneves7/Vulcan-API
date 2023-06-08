eae79ff852e5a12787c24938189200518e4f3600
NekoYasha <autornekoyasha@gmail.com>
2023-06-08 13:24:36 -0300

    Cria exceção de Lista vazia

--- src/main/java/br/com/vulcan/jvulcan/api/infrastructure/exception/EmptyListException.java
+++ src/main/java/br/com/vulcan/jvulcan/api/infrastructure/exception/EmptyListException.java
@@ -1,2 +1,19 @@
-package br.com.vulcan.jvulcan.api.exception;public class EmptyListException {
+package br.com.vulcan.jvulcan.api.infrastructure.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NO_CONTENT)
+public class EmptyListException extends EntityNotFoundException {
+
+    public EmptyListException()
+    {
+        super();
+    }
+
+    public EmptyListException(String message)
+    {
+        super(message);
+    }
 }
