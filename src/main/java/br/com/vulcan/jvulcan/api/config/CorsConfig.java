9793166870256af74e491188731a91f67ca486d1
NekoYasha <autornekoyasha@gmail.com>
2023-06-09 18:55:18 -0300

    Cria classe para configurar CORS

--- src/main/java/br/com/vulcan/jvulcan/api/config/CorsConfig.java
+++ src/main/java/br/com/vulcan/jvulcan/api/config/CorsConfig.java
@@ -1,2 +1,28 @@
-package br.com.vulcan.jvulcan.api.config;public class CorsConfig {
+package br.com.vulcan.jvulcan.api.config;
+
+import br.com.vulcan.jvulcan.api.infrastructure.origin.OrigensAutorizadas;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private String[] origens;
+
+    public CorsConfig()
+    {
+        origens = Arrays.stream(OrigensAutorizadas.values())
+                .map(OrigensAutorizadas::getUrl)
+                .toArray(String[]::new);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins(origens);
+    }
 }
