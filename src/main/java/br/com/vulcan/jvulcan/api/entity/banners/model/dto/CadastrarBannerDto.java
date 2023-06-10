97bc8ad47af5b9b8c74baa59b57fa526ade377f9
NekoYasha <autornekoyasha@gmail.com>
2023-06-10 20:34:42 -0300

    Cria dto para cadastro de banners

--- src/main/java/br/com/vulcan/jvulcan/api/entity/banners/model/dto/CadastrarBannerDto.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/banners/model/dto/CadastrarBannerDto.java
@@ -1,2 +1,24 @@
-package br.com.vulcan.jvulcan.api.entity.banners.model.dto;public class CadastrarBannerDto {
+package br.com.vulcan.jvulcan.api.entity.banners.model.dto;
+
+import br.com.vulcan.jvulcan.api.entity.novel.model.Novel;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CadastrarBannerDto
+{
+
+    @JsonProperty(value = "nome")
+    private String nome;
+
+    @JsonProperty(value = "link")
+    private String link;
+
+    @JsonProperty(value = "novel")
+    private Novel novel;
+
 }
