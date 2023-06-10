4cb8acbd4dd07b3684aa616d78a1d0cdffddb2c4
NekoYasha <autornekoyasha@gmail.com>
2023-06-10 20:36:57 -0300

    Cria exceção 'objeto já existe'

--- src/main/java/br/com/vulcan/jvulcan/api/infrastructure/exception/ObjectAlreadyExistsException.java
+++ src/main/java/br/com/vulcan/jvulcan/api/infrastructure/exception/ObjectAlreadyExistsException.java
@@ -1,2 +1,16 @@
-package br.com.vulcan.jvulcan.api.infrastructure.exception;public class ObjectAlreadyExistsException {
+package br.com.vulcan.jvulcan.api.infrastructure.exception;
+
+import jakarta.persistence.EntityExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ObjectAlreadyExistsException extends EntityExistsException
+{
+
+    public ObjectAlreadyExistsException(String message)
+    {
+        super(message);
+    }
+
 }
