a215e4b4bc391f51639cad4e4faef08fa29da44a
NekoYasha <autornekoyasha@gmail.com>
2023-06-10 20:36:11 -0300

    Cria enum com origens autorizadas

--- src/main/java/br/com/vulcan/jvulcan/api/infrastructure/origin/OrigensAutorizadas.java
+++ src/main/java/br/com/vulcan/jvulcan/api/infrastructure/origin/OrigensAutorizadas.java
@@ -1,2 +1,21 @@
-package br.com.vulcan.jvulcan.api.infrastructure.origin;public class OrigensAutorizadas {
+package br.com.vulcan.jvulcan.api.infrastructure.origin;
+
+public enum OrigensAutorizadas
+{
+
+    VULCAN("https://vulcannovel.com.br/"),
+    APILL("https://apill.vulcannovel.com.br/"),
+    LOCAL_HOST("http://localhost:3000/");
+
+    private final String url;
+
+    private OrigensAutorizadas(String url)
+    {
+        this.url = url;
+    }
+
+    public String getUrl()
+    {
+        return this.url;
+    }
 }
