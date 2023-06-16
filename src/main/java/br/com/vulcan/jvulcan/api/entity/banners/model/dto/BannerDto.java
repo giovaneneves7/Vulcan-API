ae6f726cb99422dce11a62fa9bd783955628f470
NekoYasha <autornekoyasha@gmail.com>
2023-06-16 13:42:48 -0300

    Adiciona validação de quantidade de caracter dos atributos

--- src/main/java/br/com/vulcan/jvulcan/api/entity/banners/model/dto/BannerDto.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/banners/model/dto/BannerDto.java
@@ -3,6 +3,7 @@ package br.com.vulcan.jvulcan.api.entity.banners.model.dto;
 import br.com.vulcan.jvulcan.api.entity.banners.model.Banner;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,12 +17,15 @@ import java.util.stream.Collectors;
 public class BannerDto
 {
 
+    @Size(min = 5, max = 50, message = "O nome deve ter entre 5 e 50 caracteres")
     @JsonProperty(value = "nome")
     private String nome;
 
+    @Size(min = 5, message = "O link é muito curto")
     @JsonProperty(value = "link")
     private String link;
 
+    @Size(min = 5, message = "O url de redirecionamento é muito curto")
     @JsonProperty(value = "url_redirecionamento")
     private String urlRedirecionamento;
 
@@ -33,6 +37,12 @@ public class BannerDto
 
     }
 
+    /**
+     * Converte uma lista de objetos banners em bannersDTO
+     *
+     * @param banners A lista de banners que será convertida.
+     * @return Lista de objetos convertidos para bannerDto.
+     */
     public static List<BannerDto> converterLista(List<Banner> banners)
     {
         return banners.stream().map(BannerDto::new)
