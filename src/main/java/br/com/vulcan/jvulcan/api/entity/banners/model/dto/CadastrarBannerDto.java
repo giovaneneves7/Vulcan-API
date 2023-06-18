0626dc2f5ecb3f0c0fbf7b956753d4b5de915d10
NekoYasha <autornekoyasha@gmail.com>
2023-06-18 10:58:33 -0300

    Modifica atributo 'novel' para 'urlRedirecionamento

--- src/main/java/br/com/vulcan/jvulcan/api/entity/banners/model/dto/CadastrarBannerDto.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/banners/model/dto/CadastrarBannerDto.java
@@ -1,6 +1,5 @@
 package br.com.vulcan.jvulcan.api.entity.banners.model.dto;
 
-import br.com.vulcan.jvulcan.api.entity.novel.model.Novel;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,7 +17,7 @@ public class CadastrarBannerDto
     @JsonProperty(value = "link")
     private String link;
 
-    @JsonProperty(value = "novel")
-    private Novel novel;
+    @JsonProperty(value = "url_redirecionamento")
+    private String urlRedirecionamento;
 
 }
