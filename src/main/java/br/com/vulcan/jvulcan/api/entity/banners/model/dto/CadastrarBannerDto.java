32bb9cc22d82d8d79b9ec3817db2a7539b17a6c3
NekoYasha <autornekoyasha@gmail.com>
2023-06-18 11:08:53 -0300

    Adiciona anotações de validação

--- src/main/java/br/com/vulcan/jvulcan/api/entity/banners/model/dto/CadastrarBannerDto.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/banners/model/dto/CadastrarBannerDto.java
@@ -1,6 +1,7 @@
 package br.com.vulcan.jvulcan.api.entity.banners.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +12,15 @@ import lombok.NoArgsConstructor;
 public class CadastrarBannerDto
 {
 
+    @Size(min = 5, max = 50, message = "O nome deve ter entre 5 e 50 caracteres")
     @JsonProperty(value = "nome")
     private String nome;
 
+    @Size(min = 5, message = "O link é muito curto")
     @JsonProperty(value = "link")
     private String link;
 
+    @Size(min = 5, message = "O url de redirecionamento é muito curto")
     @JsonProperty(value = "url_redirecionamento")
     private String urlRedirecionamento;
 
