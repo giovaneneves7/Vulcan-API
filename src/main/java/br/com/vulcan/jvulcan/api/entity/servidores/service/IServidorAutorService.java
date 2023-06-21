fcefc4118548431693e1f9f0815d0763c012ea68
NekoYasha <autornekoyasha@gmail.com>
2023-06-21 20:45:18 -0300

    Cria metodo para cadastrar servidores

--- src/main/java/br/com/vulcan/jvulcan/api/entity/servidores/service/IServidorAutorService.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/servidores/service/IServidorAutorService.java
@@ -1,2 +1,25 @@
-package br.com.vulcan.jvulcan.api.entity.servidores.service;public interface IServidorAutorService {
+package br.com.vulcan.jvulcan.api.entity.servidores.service;
+
+import br.com.vulcan.jvulcan.api.entity.servidores.model.ServidorAutor;
+import br.com.vulcan.jvulcan.api.entity.servidores.model.dto.CadastrarServidorAutorDto;
+import org.springframework.data.domain.Page;
+
+public interface IServidorAutorService
+{
+
+
+    /**
+     * Cadastra um servidor de autor na base de dados.
+     *
+     * @param servidorAutorDto O servidor de autor a ser cadastrado.
+     * @return informações do servidor cadastrado.
+     */
+    CadastrarServidorAutorDto cadastrarServidorAutor(CadastrarServidorAutorDto servidorAutorDto);
+
+    /**
+     * Retorna uma página com os servidores cadastrados na base de dados.
+     *
+     * @return lista com os servidores na base de dados.
+     */
+    Page<ServidorAutor> listarTodosServidores();
 }
---
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/servidores/service/ServidorAutorService.java
@@ -0,0 +1,2 @@
+package br.com.vulcan.jvulcan.api.entity.servidores.service;public class ServidorAutorService {
+}
