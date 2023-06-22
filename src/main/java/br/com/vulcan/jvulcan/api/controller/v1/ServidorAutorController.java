9cb046440eaf3a34d25114b2ed1715a3d3a976a6
NekoYasha <autornekoyasha@gmail.com>
2023-06-21 21:09:37 -0300

    Cria endpoint para salvar servidores de autor

--- src/main/java/br/com/vulcan/jvulcan/api/controller/v1/ServidorAutorController.java
+++ src/main/java/br/com/vulcan/jvulcan/api/controller/v1/ServidorAutorController.java
@@ -1,2 +1,75 @@
-package br.com.vulcan.jvulcan.api.controller.v1;public class ServidorAutorController {
+package br.com.vulcan.jvulcan.api.controller.v1;
+
+import br.com.vulcan.jvulcan.api.entity.servidores.model.dto.CadastrarServidorAutorDto;
+import br.com.vulcan.jvulcan.api.entity.servidores.service.IServidorAutorService;
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "nekoyasha7/jvulcan-api/v1")
+public class ServidorAutorController {
+
+    @Autowired
+    IServidorAutorService servidorAutorService;
+
+    @Value("${api_key}")
+    private String API_KEY;
+
+    private HashMap<String, String> erros;
+
+    @PostConstruct
+    public void init()
+    {
+        if(!API_KEY.isEmpty())
+            log.info("Endpoint usando API");
+
+        log.info("Endpoint público");
+    }
+
+    @PostMapping("/servidores-autores/servidor")
+    ResponseEntity<?> cadastrarServidorAutor(@RequestHeader(name = "Api-Key") String chaveApi,
+                                             @RequestBody @Valid CadastrarServidorAutorDto servidorAutorDto,
+                                             BindingResult result)
+    {
+        erros = new HashMap<>();
+
+        if(!chaveApi.equals(API_KEY))
+        {
+            erros.put("api_permission_error", "Você não tem autorização para acessar este endpoint, bleh!");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                                 .body(erros);
+        }
+
+        if(result.hasErrors())
+        {
+            for(FieldError campo : result.getFieldErrors())
+            {
+                erros.put(campo.getField(), campo.getDefaultMessage());
+            }
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body(erros);
+        }
+
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .body(servidorAutorService.cadastrarServidorAutor(servidorAutorDto));
+
+
+    }
 }
