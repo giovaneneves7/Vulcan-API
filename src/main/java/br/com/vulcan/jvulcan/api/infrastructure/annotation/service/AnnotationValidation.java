7b4bf0996954d957650d0b385398e4fa9d673524
NekoYasha <autornekoyasha@gmail.com>
2023-06-22 21:54:14 -0300

    Cria método para validar anotações

--- src/main/java/br/com/vulcan/jvulcan/api/infrastructure/annotation/service/AnnotationValidation.java
+++ src/main/java/br/com/vulcan/jvulcan/api/infrastructure/annotation/service/AnnotationValidation.java
@@ -1,2 +1,19 @@
-package br.com.vulcan.jvulcan.api.infrastructure.annotation;public class AnnotationValidation {
+package br.com.vulcan.jvulcan.api.infrastructure.annotation.service;
+
+import br.com.vulcan.jvulcan.api.infrastructure.annotation.ValidApiKey;
+
+public class AnnotationValidation implements IAnnotationValidation
+{
+
+    @Override
+    public boolean validarChaveApi(String chaveApi) {
+
+        ValidApiKey anotacao = chaveApi.getClass().getAnnotation(ValidApiKey.class);
+
+        if(anotacao == null)
+            throw new IllegalArgumentException("Anotação inválida");
+
+        return (anotacao.apiKey().equals(chaveApi));
+
+    }
 }
