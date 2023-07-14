207f011f2a80fe07130a846eec81621be08e3dde
NekoYasha <autornekoyasha@gmail.com>
2023-07-14 10:33:56 -0300

    Adiciona método 'encontrar pelo nome'

--- src/main/java/br/com/vulcan/jvulcan/api/entity/novel/repository/NovelRepository.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/novel/repository/NovelRepository.java
@@ -13,22 +13,22 @@ public interface NovelRepository extends JpaRepository<Novel, Long>
 
     /**
      * Retorna lista de novels com a nacionalidade especificada.
-     * @param nacionalidade A nacionalidade da novel a ser buscada.
-     * @return lista de novel com a nacionalidade especificada.
+     * @param nacionalidade A nacionalidade da model a ser buscada.
+     * @return lista de model com a nacionalidade especificada.
      */
     List<Novel> findByNacionalidade(String nacionalidade);
 
     /**
-     * Retorna uma novel com o slug passado por parâmetro.
-     * @param slug O slug da novel a ser buscada.
-     * @return novel com o slug especificado.
+     * Retorna uma model com o slug passado por parâmetro.
+     * @param slug O slug da model a ser buscada.
+     * @return model com o slug especificado.
      */
     Optional<Novel> findBySlug(String slug);
 
     /**
-     * Retorna novel com o nome passado por parâmetro.
-     * @param nome O nome da novel a ser buscado.
-     * @return novel com o nome especificado.
+     * Retorna model com o nome passado por parâmetro.
+     * @param nome O nome da model a ser buscado.
+     * @return model com o nome especificado.
      */
     Optional<Novel> findByNome(String nome);
 }
