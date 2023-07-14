c76b76c4bedf4344e82d6620385a0999161aec09
NekoYasha <autornekoyasha@gmail.com>
2023-07-14 10:48:56 -0300

    Cria entidade 'View'

--- src/main/java/br/com/vulcan/jvulcan/api/entity/view/model/View.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/view/model/View.java
@@ -1,2 +1,36 @@
-package br.com.vulcan.jvulcan.api.entity.view.model;public class View {
+package br.com.vulcan.jvulcan.api.entity.view.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "views")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class View {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String categoria;
+    @Column(name = "views_totais")
+    private long viewsTotais;
+    @Column(name = "views_mensais")
+    private long viewsMensais;
+
+    public View(String categoria, long viewsTotais, long viewsMensais) {
+        this.categoria = categoria;
+        this.viewsTotais = viewsTotais;
+        this.viewsMensais = viewsMensais;
+    }
 }
