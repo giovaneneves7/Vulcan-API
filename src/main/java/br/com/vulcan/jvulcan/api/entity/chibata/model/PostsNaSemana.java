6269f2fd9aad15e98be21f600b444ae1b221bbf7
NekoYasha <autornekoyasha@gmail.com>
2023-07-14 10:15:01 -0300

    Cria entidade 'PostsNaSemana'

--- src/main/java/br/com/vulcan/jvulcan/api/entity/chibata/model/PostsNaSemana.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/chibata/model/PostsNaSemana.java
@@ -1,2 +1,37 @@
-package br.com.vulcan.jvulcan.api.entity.chibata.model;public class PostsNaSemana {
+package br.com.vulcan.jvulcan.api.entity.chibata.model;
+
+import br.com.vulcan.jvulcan.api.entity.novel.model.Novel;
+import br.com.vulcan.jvulcan.api.entity.post.model.Post;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+import java.util.List;
+
+@Entity
+@Table(name = "posts_na_semana")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostsNaSemana {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @OneToOne
+    private Novel novel;
+
+    private int totalPosts;
+
+    @OneToMany
+    private List<Post> posts;
+
 }
