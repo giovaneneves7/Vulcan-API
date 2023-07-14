1a6a141a0252a4bb2127c4948739389708184934
NekoYasha <autornekoyasha@gmail.com>
2023-07-14 10:27:44 -0300

    Cria repositória para a entidade PostsNaSemana

--- src/main/java/br/com/vulcan/jvulcan/api/entity/chibata/repository/PostsNaSemanaRepository.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/chibata/repository/PostsNaSemanaRepository.java
@@ -1,2 +1,16 @@
-package br.com.vulcan.jvulcan.api.entity.chibata.repository;public interface PostsNaSemanaRepository {
+package br.com.vulcan.jvulcan.api.entity.chibata.repository;
+
+import br.com.vulcan.jvulcan.api.entity.chibata.model.PostsNaSemana;
+import br.com.vulcan.jvulcan.api.entity.novel.model.Novel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface PostsNaSemanaRepository extends JpaRepository<PostsNaSemana, UUID> {
+
+    public Optional<PostsNaSemana> findByNovel(Novel novel);
+
 }
