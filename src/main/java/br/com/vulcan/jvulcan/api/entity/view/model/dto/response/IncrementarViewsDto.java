1ecb25c96365f3e34c772abe0da9116720e81bf7
NekoYasha <autornekoyasha@gmail.com>
2023-07-19 21:29:44 -0300

    Edita dependências

--- src/main/java/br/com/vulcan/jvulcan/api/entity/view/model/dto/response/IncrementarViewsDto.java
+++
@@ -1,9 +0,0 @@
-package br.com.vulcan.jvulcan.api.entity.view.model.dto.response;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record IncrementarViewsDto(@JsonProperty(value = "categoria")
-                                  String categoria,
-                                  @JsonProperty(value = "total_views")
-                                  long totalViews){
-}
