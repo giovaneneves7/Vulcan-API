f528e3402a7f83879dcebbb9f6db61db0e14c38f
NekoYasha <autornekoyasha@gmail.com>
2023-08-05 13:34:45 -0300

    Remove estratégia de geração de ID

--- src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/Novel.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/Novel.java
@@ -20,7 +20,7 @@ public class Novel {
 
     @Column(name = "id")
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     @Column(name = "colocacao_total")
