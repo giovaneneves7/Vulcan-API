abb96a623c8c0fdc442ff2e4bcf6329ab9c8a5c5
NekoYasha <autornekoyasha@gmail.com>
2023-08-12 12:51:43 -0300

    Adiciona DTOs de resposta para o método 'buscar por slug'

--- src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/dto/response/NovelComRankDto.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/dto/response/NovelComRankDto.java
@@ -1,2 +1,24 @@
-package br.com.vulcan.jvulcan.api.entity.novel.model.dto.response;public class NovelComRankDto {
+package br.com.vulcan.jvulcan.api.entity.novel.model.dto.response;
+
+import br.com.vulcan.jvulcan.api.entity.novel.model.Novel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class NovelComRankDto extends NovelResponseDto{
+
+    @JsonProperty(value = "colocacao_total")
+    int colocacaoTotal;
+
+    @JsonProperty(value = "colocacao_mensal")
+    int colocacaoMensal;
+
+    public NovelComRankDto(Novel novel){
+
+        this.slug = novel.getSlug();
+        this.nome = novel.getNome();
+        this.colocacaoTotal = novel.getColocacao();
+        this.colocacaoMensal = novel.getColocacaoMensal();
+
+    }
+
 }
--- src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/dto/response/NovelResponseDto.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/dto/response/NovelResponseDto.java
@@ -1,2 +1,19 @@
-package br.com.vulcan.jvulcan.api.entity.novel.model.dto.response;public class NovelResponseDto {
+package br.com.vulcan.jvulcan.api.entity.novel.model.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NovelResponseDto {
+
+    @JsonProperty(value = "novel")
+    public String nome;
+
+    @JsonProperty(value = "slug")
+    public String slug;
+
 }
