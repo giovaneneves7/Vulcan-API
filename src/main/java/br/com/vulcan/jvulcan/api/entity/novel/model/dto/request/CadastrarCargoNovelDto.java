a6b51600068818a84b76a29da4c4b866f0650157
NekoYasha <autornekoyasha@gmail.com>
2023-08-19 11:10:48 -0300

    Cria DTO de requisição para o cadastro de cargos Criado objeto que contém os dados da novel que terá o cargo atualizado.

--- src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/dto/request/CadastrarCargoNovelDto.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/dto/request/CadastrarCargoNovelDto.java
@@ -1,2 +1,18 @@
-package br.com.vulcan.jvulcan.api.entity.novel.model.dto.request;public class CadastrarCargoNovelDto {
+package br.com.vulcan.jvulcan.api.entity.novel.model.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CadastrarCargoNovelDto (
+        @JsonProperty(value = "cargo")
+        @NotNull
+        @NotBlank
+        String cargo,
+
+        @JsonProperty(value = "id")
+        @NotNull
+        String id
+){
+
 }
