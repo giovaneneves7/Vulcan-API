0ae5889f3293c8be146a12d7cd705f3bce8a9475
NekoYasha <autornekoyasha@gmail.com>
2023-08-19 11:09:35 -0300

    Cria DTO de resposta para o cadastro de cargos Criado DTO que retorna informações da novel que teve o cargo cadastrado.

--- src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/dto/response/NovelComCargoDto.java
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/dto/response/NovelComCargoDto.java
@@ -1,2 +1,7 @@
-package br.com.vulcan.jvulcan.api.entity.novel.model.dto.response;public class NovelComCargoDto {
+package br.com.vulcan.jvulcan.api.entity.novel.model.dto.response;
+
+public record NovelComCargoDto(
+        String nome,
+        String cargo
+) {
 }
