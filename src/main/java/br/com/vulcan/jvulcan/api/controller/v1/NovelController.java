21a6e661488b2c4d3702aba40c26ac91252686c8
NekoYasha <autornekoyasha@gmail.com>
2023-09-04 10:29:32 -0300

    Cria endpoint 'PUT' par atualizar rankings das novels

--- src/main/java/br/com/vulcan/jvulcan/api/controller/v1/NovelController.java
+++ src/main/java/br/com/vulcan/jvulcan/api/controller/v1/NovelController.java
@@ -14,18 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -102,6 +96,40 @@ public class NovelController
 
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping(path = "/novels/views")
+    public ResponseEntity<?> atualizarViews(@RequestHeader(name = "Api-Key") String chaveApi){
+
+        erros = new HashMap<>();
+
+        if(!chaveApi.equals(API_KEY)){
+
+            log.error("Acesso negado ao endpoint de cadastro!");
+            erros.put(Errors.API_PERMISSION_ERROR.getKey(), Errors.API_PERMISSION_ERROR.getErro());
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(erros);
+        }
+
+        this.facade.atualizarViewsDasNovels();
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Novels atualizadas!");
+
+    }
+
+    @PutMapping(path = "/novels/rankings/total")
+    public ResponseEntity<?> atualizarRankings(@RequestHeader(name = "Api-Key") String chaveApi){
+
+        if(!chaveApi.equals(API_KEY)){
+            log.error("Acesso negado ao endpoint de cadastro!");
+            erros.put(Errors.API_PERMISSION_ERROR.getKey(), Errors.API_PERMISSION_ERROR.getErro());
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(erros);
+        }
+
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(this.facade.atualizarRankingTotalDasNovels());
+    }
+
     @GetMapping(path = "/novels/novel/{slug}")
     public ResponseEntity<?> pegarNovelPorSlug(@PathVariable(name = "slug") String slug,
                                                @RequestHeader(name = "Api-Key") String chaveApi,
@@ -112,14 +140,6 @@ public class NovelController
 
     }
 
-    /*@CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PutMapping("novels/novel")
-    public ResponseEntity<List<Novel>> atualizarCargo(@RequestBody List<Cargo> cargos)
-    {
-
-        return ResponseEntity.ok(this.facade.atualizarCargoDasNovels(cargos));
-
-    }*/
     @CrossOrigin(origins = {"http://localhost:3000", "https://apill.vulcannovel.com.br", "https://vulcannovel.com.br"}, allowedHeaders = "Content-Type")
     @PostMapping("/novels/novel")
     public ResponseEntity<?> cadastrarNovel(@RequestBody CadastrarNovelDto novelDto,
---
+++ src/main/java/br/com/vulcan/jvulcan/api/entity/novel/model/dto/response/NovelComRankingTotalAtualizadoDto.java
@@ -0,0 +1,2 @@
+package br.com.vulcan.jvulcan.api.entity.novel.model.dto.response;public record NovelComRankingAtualizadoDto() {
+}
