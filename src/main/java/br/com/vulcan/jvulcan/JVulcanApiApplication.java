458d6bef04ba863c918132247390f75502cdf1ff
NekoYasha <autornekoyasha@gmail.com>
2023-09-04 09:51:28 -0300

    Adiciona anotação 'enableScheduling'

--- src/main/java/br/com/vulcan/jvulcan/JVulcanApiApplication.java
+++ src/main/java/br/com/vulcan/jvulcan/JVulcanApiApplication.java
@@ -3,9 +3,11 @@ package br.com.vulcan.jvulcan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class JVulcanApiApplication {
 
     public static void main(String[] args) {
